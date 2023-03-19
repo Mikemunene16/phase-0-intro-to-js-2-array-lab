bca55f597faa9bfbdc9873fe6d17a3ee4a3e64ef
Mike <mike.munene@student.moringaschool.com>
2023-03-19 19:39:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+// function destructivelyAppendCat(name) {
+//     return name.push('Ralph');
+// }
+const destructivelyAppendCat = function (name) {
+    return cats.push(name);
+}
+
+const destructivelyPrependCat = function (name) {
+    return cats.unshift(name);
+}
+
+const destructivelyRemoveLastCat = function (name) {
+    return cats.pop(name);
+}
+
+const destructivelyRemoveFirstCat = function (name) {
+    return cats.shift(name);
+}
+
+const appendCat = function (name) {
+    return [...cats, name];
+}
+
+const prependCat = function (name) {
+    return [name, ...cats];
+}
+
+const removeLastCat = function () {
+    return cats.slice(0, cats.length - 1);
+}
+
+const removeFirstCat = function () {
+    return cats.slice(1);
+}
